e90ee66c93a264e89b90982b022446b2e1d11891
superCODZE <mazathomazigh@gmail.com>
2026-09-06 22:00:39 +0100

    add some cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,16 +2,20 @@
 
 function Card({ title, description, imageUrl }) {
   return (
+    <>
+    <div className="card-hover">
     <div className="card">
       <div className="card-title">
          <img src={imageUrl} alt={title} className="card-image" />
-         <h2 >{title}</h2>
+         <h3>{title}</h3>
       </div>
-      
+    
       <div className="card-content">
          <p className="card-description">{description}</p>
       </div>
     </div>
+    </div>
+    </>
   );
 }
 
--- src/components/skillsSection.jsx
+++ src/components/skillsSection.jsx
@@ -7,14 +7,15 @@ function SkillsSection() {
     <>
       <section id="skills" className="skills-section">
         <div className="skills-header">
-          <h2>My Skills</h2>
-          <p>Here are some of the skills I have acquired over the years</p>
+          <h1>My Skills</h1>
+          <p> Here are some of the skills I have acquired over the years </p>
         </div>
 
         <div className="skills-cards">
-          <Card title="Python" description="Proficient in Python programming and backend development." imageUrl="public/python-pixel-transparent.png" />
-          <Card title="Java" content="Experienced in Java programming and object-oriented design." imageUrl="public/java-pixel.png" />
-          
+          <Card title="Python" description="Proficient in Python programming and backend development." imageUrl="/python.svg" />
+          <Card title="Java" description="Experienced in Java programming and object-oriented design." imageUrl="/java.svg" />
+          <Card title="C++" description="Skilled in C++ programming and system-level development." imageUrl="/cpp.svg" />
+         
         </div>
 
       </section>
