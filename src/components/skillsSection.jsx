0a90fa540a0c66da8a17888239e729a84c9c663a
superCODZE <mazathomazigh@gmail.com>
2026-09-07 21:03:37 +0100

    add authors cards

--- src/components/skillsSection.jsx
+++ src/components/skillsSection.jsx
@@ -13,8 +13,11 @@ function SkillsSection() {
 
         <div className="skills-cards">
           <Card title="Python" description="Proficient in Python programming and backend development." imageUrl="/python.svg" />
+        
           <Card title="Java" description="Experienced in Java programming and object-oriented design." imageUrl="/java.svg" />
           <Card title="C++" description="Skilled in C++ programming and system-level development." imageUrl="/cpp.svg" />
+          <Card title="Css" description="Proficient in CSS for styling web applications and creating responsive designs." imageUrl="/css.png" />
+          <Card title="Html" description="Experienced in HTML for structuring web content and creating semantic markup." imageUrl="/html.png" />
          
         </div>
 
