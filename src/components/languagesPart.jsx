dc4736c3af5d69457f1a6efcd79b623751333011
superCODZE <mazathomazigh@gmail.com>
2026-09-14 22:03:32 +0100

    return to work

---
+++ src/components/languagesPart.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+function LanguagesPart({Title}) {
+    return (
+    <>
+    <div className="skills-part">
+
+        <h2>{Title}</h2>
+        <div className="languages-icons">
+           
+
+        </div>
+        
+
+    </div>
+    </>
+    );
+}
+
+export default LanguagesPart;
--- src/components/skillsSection.jsx
+++ src/components/skillsSection.jsx
@@ -1,5 +1,6 @@
 
 import Card from './card';
+import LanguagesPart from './languagesPart';
 
 
 function SkillsSection() {
@@ -11,14 +12,12 @@ function SkillsSection() {
           <p> Here are some of the skills I have acquired over the years </p>
         </div>
 
-        <div className="skills-cards">
-          <Card title="Python" description="Proficient in Python programming and backend development." imageUrl="/python.svg" />
-        
-          <Card title="Java" description="Experienced in Java programming and object-oriented design." imageUrl="/java.svg" />
-          <Card title="C++" description="Skilled in C++ programming and system-level development." imageUrl="/cpp.svg" />
-          <Card title="Css" description="Proficient in CSS for styling web applications and creating responsive designs." imageUrl="/css.png" />
-          <Card title="Html" description="Experienced in HTML for structuring web content and creating semantic markup." imageUrl="/html.png" />
-         
+        <div className="skills-info">
+          <LanguagesPart Title="Programming Languages" />
+          <LanguagesPart Title="Frameworks & Libraries" />
+          <LanguagesPart Title="Databases" />
+          <LanguagesPart Title="Tools & Platforms" />
+          
         </div>
 
       </section>
